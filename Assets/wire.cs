1cb3a95c9d28a2e6feb6ac2284a67f34e20117d2
Kakami Souma <nkc.souma.kakami@gmail.com>
2019-07-01 09:48:00 +0900

    add:プロジェクト追加

---
+++ Assets/wire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wire : MonoBehaviour
+{
+	Rigidbody rigidbody;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rigidbody = transform.parent.GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+	void RayHitWall()
+	{
+		Ray ray = new Ray(transform.position);
+	}
+}
