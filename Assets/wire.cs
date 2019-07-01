ab43e1452b85b32ed3732d844aaf2edcfa3ac17b
Kakami Souma <nkc.souma.kakami@gmail.com>
2019-07-01 11:47:42 +0900

    add:動くよ

--- Assets/wire.cs
+++ Assets/wire.cs
@@ -5,20 +5,50 @@ using UnityEngine;
 public class wire : MonoBehaviour
 {
 	Rigidbody rigidbody;
-
+	public GameObject cam;
+	Vector3 left;
+	Vector3 right;
+	public GameObject t;
 	// Start is called before the first frame update
 	void Start()
 	{
-		rigidbody = transform.parent.GetComponent<Rigidbody>();
+		rigidbody = transform.GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
+		RayHitWall();
 	}
 	void RayHitWall()
 	{
-		Ray ray = new Ray(transform.position);
+		Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+		RaycastHit hit=new RaycastHit();
+		Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.red, 0.0f);
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (Physics.Raycast(ray, out hit, 100.0f))
+			{
+				right = hit.point - transform.position;
+				right.Normalize();
+				Debug.Log(right);
+				Instantiate(t, hit.point, Quaternion.identity);
+			}
+		}
+		if (Input.GetMouseButtonDown(1))
+		{
+			if (Physics.Raycast(ray, out hit, 100.0f))
+			{
+				left = hit.point-transform.position;
+				left.Normalize();
+				Debug.Log(left);
+				Instantiate(t,hit.point,Quaternion.identity);
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody.AddForce((left+right)*10.0f,ForceMode.Impulse);
+		}
 	}
 }
